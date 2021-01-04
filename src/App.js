e177e2a08d74f63a55c3b551aa4fd07ceb9bf402
asherkim <asherkim@b2link.co.kr>
2021-01-04 23:42:13 +0900

    create-react-app 으로 시작하기 동적으로 데이터를 가져오기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React,  { useEffect  } from 'react';
 import logo from './logo.svg';
+// import data from './data.json';
 import './App.css';
 
 function App() {
+  function onClick() {
+    // console.log(data);
+    // 동적으로 데이터를 가져오기
+    import('./data.json').then(({default: data}) => {
+      console.log({data});
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello~</p>
+        <button onClick={onClick}>데이터를 보여주세요.</button>
+        <p>hello!!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
